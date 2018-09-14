1524148c45a2488267d8a7edc805753ca804b067
Mathilde Loiselet <mathilde.loiselet@gmail.com>
2018-09-14 10:06:16 +0200

    debut creation deux premiere page projet

--- app/src/main/java/com/wildcodeschool/monsterlegendwiki/CategorieMonstres.java
+++ app/src/main/java/com/wildcodeschool/monsterlegendwiki/CategorieMonstres.java
@@ -1,7 +1,10 @@
 package com.wildcodeschool.monsterlegendwiki;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CategorieMonstres extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class CategorieMonstres extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categorie_monstres);
+
+        final Button button_monstres = findViewById(R.id.buttonmonstres);
+        button_monstres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CategorieMonstres.this, ListChooseMonsters.class);
+                startActivity(intent);
+            }
+        });
     }
 }
